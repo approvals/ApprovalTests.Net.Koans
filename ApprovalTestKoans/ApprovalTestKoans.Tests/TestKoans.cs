b1a1c2adf7b0b25f9b24cd34026e303351b13c03
Jim Counts <jamesrcounts@outlook.com>
2013-10-01 16:07:46 -0700

    combo approvals

--- ApprovalTestKoans/ApprovalTestKoans.Tests/TestKoans.cs
+++ ApprovalTestKoans/ApprovalTestKoans.Tests/TestKoans.cs
@@ -9,90 +9,101 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ApprovalTestKoans.Tests
 {
-	[TestClass]
-	public class TestKoans
-	{
-		[TestMethod]
-		public void TestGettingStarted()
-		{
-			VerifyKoan<GettingStarted>(k => k.NormalAsserts, "Small String");
-			VerifyKoan<GettingStarted>(k => k.ApprovalHidesTheExpectation, "This is in the approved file");
-			VerifyKoan<GettingStarted>(k => k.ApprovalFileName, "GettingStarted.ApprovalFileName");
-			VerifyKoanIsUnsolved<GettingStarted>(k => k._____);
-			VerifyKoan<GettingStarted>(k => k.FileNames, "GettingStarted");
-			VerifyKoan<GettingStarted>(k => k.SeeingFilesSideBySide, "This file is called");
-			VerifyKoanIsUnsolved<GettingStarted>(k => k.ChangingTheGoldenMaster);
-			VerifyKoan<GettingStarted>(k => k.VerifyObjects, 150);
-			VerifyKoan<GettingStarted>(k => k.SometimeYouNeedABetterToString, "Cobb");
-		}
+    using Lesson05;
 
-		[TestMethod]
-		public void TestCollections()
-		{
-			VerifyKoan<Collections>(k => k.SimpleArrays, ".Net");
-			VerifyKoan<Collections>(k => k.ListsWorkTheSameAsArrays, "Collections");
-			VerifyKoan<Collections>(k => k.DictionariesAreSortedBeforeDisplay, "L");
-			VerifyKoan<Collections>(k => k.TheUseOfTheLabel, "TV Show");
-			VerifyKoan<Collections>(k => k.ApprovalHelpShowWhenThingsHaveBeenRemoved, "Mr. Green");
-			VerifyKoanIsUnsolved<Collections>(k => k.ApprovalHelpShowWhenThingsHaveBeenAdded);
-			VerifyKoan<Collections>(k => k.HeadersHelpExplainTheContextWhenThingsAreConfusing,
-			                        "Numbers in the Fibonacci sequence");
-			VerifyKoan<Collections>(k => k.TransformingArraysManually, 5);
-			VerifyKoan<Collections>(k => k.TransformingArraysWithLambdas, "{0}^2 = {1}");
-		}
+    [TestClass]
+    public class TestKoans
+    {
+        [TestMethod]
+        public void TestGettingStarted()
+        {
+            VerifyKoan<GettingStarted>(k => k.NormalAsserts, "Small String");
+            VerifyKoan<GettingStarted>(k => k.ApprovalHidesTheExpectation, "This is in the approved file");
+            VerifyKoan<GettingStarted>(k => k.ApprovalFileName, "GettingStarted.ApprovalFileName");
+            VerifyKoanIsUnsolved<GettingStarted>(k => k._____);
+            VerifyKoan<GettingStarted>(k => k.FileNames, "GettingStarted");
+            VerifyKoan<GettingStarted>(k => k.SeeingFilesSideBySide, "This file is called");
+            VerifyKoanIsUnsolved<GettingStarted>(k => k.ChangingTheGoldenMaster);
+            VerifyKoan<GettingStarted>(k => k.VerifyObjects, 150);
+            VerifyKoan<GettingStarted>(k => k.SometimeYouNeedABetterToString, "Cobb");
+        }
 
-		[TestMethod]
-		public void TestApprovingTheResult()
-		{
-			VerifyKoanIsUnsolved<ApprovingTheResult>(k => k.RenameTheApproved);
-			VerifyKoanIsUnsolved<ApprovingTheResult>(k => k.SaveAsApproved);
-			VerifyKoanIsUnsolved<ApprovingTheResult>(k => k.MoveTheFileViaCommandLine);
-			VerifyKoanIsUnsolved<ApprovingTheResult>(k => k.CopyAndPasteContentInVisualStudio);
-			VerifyKoanIsUnsolved<ApprovingTheResult>(k => k.UseWholeFileInADiffReporter);
-		}
+        [TestMethod]
+        public void TestCollections()
+        {
+            VerifyKoan<Collections>(k => k.SimpleArrays, ".Net");
+            VerifyKoan<Collections>(k => k.ListsWorkTheSameAsArrays, "Collections");
+            VerifyKoan<Collections>(k => k.DictionariesAreSortedBeforeDisplay, "L");
+            VerifyKoan<Collections>(k => k.TheUseOfTheLabel, "TV Show");
+            VerifyKoan<Collections>(k => k.ApprovalHelpShowWhenThingsHaveBeenRemoved, "Mr. Green");
+            VerifyKoanIsUnsolved<Collections>(k => k.ApprovalHelpShowWhenThingsHaveBeenAdded);
+            VerifyKoan<Collections>(k => k.HeadersHelpExplainTheContextWhenThingsAreConfusing,
+                                    "Numbers in the Fibonacci sequence");
+            VerifyKoan<Collections>(k => k.TransformingArraysManually, 5);
+            VerifyKoan<Collections>(k => k.TransformingArraysWithLambdas, "{0}^2 = {1}");
+        }
 
-		[TestMethod]
-		public void TestUsingReporters()
-		{
-			VerifyKoanIsUnsolved<ConfiguringReporters>(k => k.ConfiguringTheFileLauncherReporterAtTheMethodLevel);
-			VerifyKoanIsUnsolved<ConfiguringReporters>(k => k.ConfiguringAClassLevelDefault);
-			VerifyKoanIsUnsolved<ConfiguringReportersPart2>(k => k.ConfiguringAnAssemblyLevelDefault);
-		//	VerifyKoan<UsingReporters>(k => k.UsingTextReportersForInsight, "Forty Two");
-		//	VerifyKoan<UsingReporters>(k => k.UsingImageForInsight, "blue");
-		}
+        [TestMethod]
+        public void TestApprovingTheResult()
+        {
+            VerifyKoanIsUnsolved<ApprovingTheResult>(k => k.RenameTheApproved);
+            VerifyKoanIsUnsolved<ApprovingTheResult>(k => k.SaveAsApproved);
+            VerifyKoanIsUnsolved<ApprovingTheResult>(k => k.MoveTheFileViaCommandLine);
+            VerifyKoanIsUnsolved<ApprovingTheResult>(k => k.CopyAndPasteContentInVisualStudio);
+            VerifyKoanIsUnsolved<ApprovingTheResult>(k => k.UseWholeFileInADiffReporter);
+        }
 
-		private void VerifyKoanIsUnsolved<T>(Func<T, Action> method) where T : Koans, new()
-		{
-			RunKoan(method, new T(), pass: false);
-		}
+        [TestMethod]
+        public void TestUsingReporters()
+        {
+            VerifyKoanIsUnsolved<ConfiguringReporters>(k => k.ConfiguringTheFileLauncherReporterAtTheMethodLevel);
+            VerifyKoanIsUnsolved<ConfiguringReporters>(k => k.ConfiguringAClassLevelDefault);
+            VerifyKoanIsUnsolved<ConfiguringReportersPart2>(k => k.ConfiguringAnAssemblyLevelDefault);
 
-		private void VerifyKoan<T>(Func<T, Action> method, object answer) where T : Koans, new()
-		{
-			VerifyKoanIsUnsolved(method);
-			var k = new T();
-			k.___ = answer as string;
-			k.____ = (int) (answer is int ? answer : 0);
-			RunKoan(method, k, pass: true);
-		}
+            //	VerifyKoan<UsingReporters>(k => k.UsingTextReportersForInsight, "Forty Two");
+            //	VerifyKoan<UsingReporters>(k => k.UsingImageForInsight, "blue");
+        }
 
+        [TestMethod]
+        public void TestDataDrivenCombinations()
+        {
+            this.VerifyKoan<DataDrivenCombinations>(k => k.OneDimensionalInputIsTheSameAsArray, "d");
+            this.VerifyKoan<DataDrivenCombinations>(k => k.TwoDimensionalInputGoesByTheSquare, 33);
+            this.VerifyKoan<DataDrivenCombinations>(k => k.ExceptionsAreOk, -2);
+            this.VerifyKoanIsUnsolved<DataDrivenCombinations>(k => k.WorkingWithALambda);
+        }
 
-		private static void RunKoan<T>(Func<T, Action> method, T k, bool pass) where T : Koans, new()
-		{
-			Exception exception = ExceptionUtilities.GetException(() => method(k).Invoke());
-			if (pass)
-			{
-				if (exception != null)
-				{
-					throw new Exception("Did Not Pass", exception);
-				}
-			}
-			else
-			{
-				if (exception == null)
-				{
-					Assert.Fail("The method {0} is already passing".FormatWith(method.Method.Name));
-				}
-			}
-		}
-	}
+        private void VerifyKoanIsUnsolved<T>(Func<T, Action> method) where T : Koans, new()
+        {
+            RunKoan(method, new T(), pass: false);
+        }
+
+        private void VerifyKoan<T>(Func<T, Action> method, object answer) where T : Koans, new()
+        {
+            VerifyKoanIsUnsolved(method);
+            var k = new T();
+            k.___ = answer as string;
+            k.____ = (int)(answer is int ? answer : 0);
+            RunKoan(method, k, pass: true);
+        }
+
+        private static void RunKoan<T>(Func<T, Action> method, T k, bool pass) where T : Koans, new()
+        {
+            Exception exception = ExceptionUtilities.GetException(() => method(k).Invoke());
+            if (pass)
+            {
+                if (exception != null)
+                {
+                    throw new Exception("Did Not Pass", exception);
+                }
+            }
+            else
+            {
+                if (exception == null)
+                {
+                    Assert.Fail("The method {0} is already passing".FormatWith(method.Method.Name));
+                }
+            }
+        }
+    }
 }
---
+++ ApprovalTestKoans/ApprovalTestKoans/Lesson05/DataDrivenCombinations.cs
@@ -0,0 +1,69 @@
+﻿namespace ApprovalTestKoans.Lesson05
+{
+    using System;
+    using ApprovalTests.Combinations;
+    using ApprovalTests.Reporters;
+    using Helpers;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /**
+            * How to do Koans:
+            * Step 1: Run Test Ctrl+R, (Not Ctrl)T (Click on the method to only run that test)
+            * TroubleShoot : Ctrl+R, Ctrl+T will run in debug mode.
+            * Step 2: Read the name of the Method that Failed, try to run 1 at a time.
+            * Step 3: Fill in the blank (___) to make it pass
+            * TroubleShoot: Do not change anything except the blank (___)
+            * Step 4: Reflect for Enlightenment
+          * Step 5: Continue
+            *
+            * NOTE: Each example builds on one another
+            *
+            */
+
+    [UseReporter(typeof(VisualStudioReporter))]
+    [TestClass]
+    public class DataDrivenCombinations : Koans
+    {
+        [TestMethod]
+        public void OneDimensionalInputIsTheSameAsArray()
+        {
+            string[] letters = { "a", "b", "c", ___, "e" };
+            CombinationApprovals.VerifyAllCombinations(a => a, letters);
+        }
+
+        [TestMethod]
+        public void TwoDimensionalInputGoesByTheSquare()
+        {
+            int[] small = { 1, 2, 3 };
+            int[] big = { 11, ____, 55 };
+
+            CombinationApprovals.VerifyAllCombinations((a, b) => a * b, small, big);
+        }
+
+        [TestMethod]
+        public void ExceptionsAreOk()
+        {
+            int[] numerator = { 60, 126, 42 };
+            int[] denominator1 = { 3, 2 };
+            int[] denominator2 = { -1, ____, 1 };
+
+            CombinationApprovals.VerifyAllCombinations((a, b, c) => a / (b + c), numerator, denominator1, denominator2);
+        }
+
+        [TestMethod]
+        public void WorkingWithALambda()
+        {
+            var newMillenium = new DateTime(2000, 1, 1, 0, 0, 0);
+            string[] years = { "yy", "yyyy" };
+            string[] months = { "M", "MMM", "MMMM" };
+            string[] days = { "dd", "dddd" };
+
+            CombinationApprovals.VerifyAllCombinations((y, m, d) => newMillenium.ToString(y + "/" + ___ + "/" + d), years, months, days);
+        }
+
+        public DataDrivenCombinations()
+        {
+            ___ = "'" + ___ + "'";
+        }
+    }
+}
